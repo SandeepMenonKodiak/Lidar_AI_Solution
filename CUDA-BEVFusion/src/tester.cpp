1478fce6e451f260e58bb12e24d4d3ed72bf9f9a
Sandeep Menon <s.menon@kodiak.ai>
2023-08-16 02:12:53 +0000

    test_camsegdecoder and test_lidarsegdecoder

--- CUDA-BEVFusion/src/tester.cpp
+++ CUDA-BEVFusion/src/tester.cpp
@@ -100,18 +100,69 @@ void visualize_map(const std::string& fpath, const bevfusion::head::mapsegm::Can
     stbi_write_jpg(fpath.c_str(), width, height, 3, &image[0], 100);
 }
 
-bool test_head(const std::string& model, const std::string& precision, const std::string& data) {
+
+bevfusion::head::mapsegm::CanvasOutput copyToCanvasOutput(const nvtype::half* data, const std::vector<int64_t>& shape) {
+    bevfusion::head::mapsegm::CanvasOutput output;
+    int batch_size = shape[0];
+    int channels = shape[1];
+    int height = shape[2];
+    int width = shape[3];
+
+    half* out_data = nullptr;
+    checkRuntime(cudaMallocHost(&out_data, batch_size * channels * height * width * sizeof(half)));
+    checkRuntime(cudaMemcpy(out_data, data, batch_size * channels * height * width * sizeof(half), cudaMemcpyDeviceToHost));
+
+    output.resize(batch_size);
+    for(int b = 0; b<batch_size;++b){
+        output[b].resize(channels);
+        for(int c = 0; c<channels;++c){
+            output[b][c].resize(height);
+            for(int h = 0; h<height;++h){
+                output[b][c][h].resize(width);
+                for(int w = 0; w<width;++w){
+                    int idx = b*channels*height*width + c*height*width + h*width + w;
+                    output[b][c][h][w] = __half2float(out_data[idx]);
+                }
+            }
+        }
+    }
+
+    if(out_data)
+        checkRuntime(cudaFreeHost(out_data));
+
+    return output;
+}
+
+bool areCanvasOutputsClose(const bevfusion::head::mapsegm::CanvasOutput v1, const bevfusion::head::mapsegm::CanvasOutput v2, float epsilon) {
+    if (v1.size() != v2.size()) return false;
+    for (size_t i = 0; i < v1.size(); ++i) {
+        if (v1[i].size() != v2[i].size()) return false;
+        for (size_t j = 0; j < v1[i].size(); ++j) {
+            if (v1[i][j].size() != v2[i][j].size()) return false;
+            for (size_t k = 0; k < v1[i][j].size(); ++k) {
+                if (v1[i][j][k].size() != v2[i][j][k].size()) return false;
+                for (size_t l = 0; l < v1[i][j][k].size(); ++l) {
+                    if (std::abs(v1[i][j][k][l] - v2[i][j][k][l]) > epsilon) return false;
+                }
+            }
+        }
+    }
+    return true;
+}
+
+
+bool test_seghead(const std::string& model, const std::string& precision, const std::string& data) {
     
     try {
       // Dummy data
       auto input_data = nv::Tensor::load(nv::format("%s/head_sig_input.tensor", data.c_str()), false);
+      auto output = nv::Tensor::load(nv::format("%s/head_sig_output.tensor", data.c_str()), false);
       const nvtype::half* input_tensor_ptr = input_data.ptr<nvtype::half>();
 
       // Load model
       bevfusion::head::mapsegm::MapSegHeadParameter mapsegm;
       mapsegm.model = nv::format("model/%s/build/head_sig.map.plan", model.c_str());
-      std::shared_ptr<bevfusion::head::mapsegm::MapSegHead> mapsegm_;
-      mapsegm_ = bevfusion::head::mapsegm::create_mapseghead(mapsegm);
+      std::shared_ptr<bevfusion::head::mapsegm::MapSegHead> mapsegm_ = bevfusion::head::mapsegm::create_mapseghead(mapsegm);
       std::vector<int> input_shape = mapsegm_->input_shape();
 
       //Forward pass
@@ -125,12 +176,130 @@ bool test_head(const std::string& model, const std::string& precision, const std
 
       cudaStreamDestroy(_stream);
 
-      return true;
+      bevfusion::head::mapsegm::CanvasOutput true_maps = copyToCanvasOutput(output.ptr<nvtype::half>(), output.shape);
+
+      return areCanvasOutputsClose(maps, true_maps, 1e-1);
     }
     catch (const std::exception& e) {
       std::cerr << e.what() << std::endl;
       return false;
     }
+}
+
+bool test_camsegdecoder(const std::string& model, const std::string& precision, const std::string& data) {
+  try{
+    // Dummy data
+    auto input_data = nv::Tensor::load(nv::format("%s/camera_bev_features.tensor", data.c_str()), false);
+    auto output_data = nv::Tensor::load(nv::format("%s/head_sig_input.tensor", data.c_str()), false);
+    std::vector<int64_t> input_shape = input_data.shape;
+    std::vector<int64_t> output_shape = output_data.shape;
+    for(int i = 0; i < input_shape.size(); i++){
+      printf("input_shape[%d] is: %ld\n", i, input_shape[i]);
+    }
+    for(int i = 0; i < output_shape.size(); i++){
+      printf("output_shape[%d] is: %ld\n", i, output_shape[i]);
+    }
+    const nvtype::half* camera_bevfeat = input_data.ptr<nvtype::half>();
+
+    // Load model
+    const std::string cameradecoder_param = nv::format("model/%s/build/camera_decoder.plan", model.c_str());
+    std::shared_ptr<bevfusion::fuser::CameraDecoder> cameradecoder_ = bevfusion::fuser::create_cameradecoder(cameradecoder_param);
+
+    //Forward pass
+    cudaStream_t _stream;
+    cudaStreamCreate(&_stream);
+
+    const nvtype::half* cameradecoder_feature = cameradecoder_->forward(camera_bevfeat, _stream);
+    checkRuntime(cudaStreamSynchronize(_stream));
+    cudaStreamDestroy(_stream);
+
+    // Compare output
+    bevfusion::head::mapsegm::CanvasOutput model_out = copyToCanvasOutput(cameradecoder_feature, output_shape);
+    bevfusion::head::mapsegm::CanvasOutput output = copyToCanvasOutput(output_data.ptr<nvtype::half>(), output_shape);
+    int batch_size = output_shape[0];
+    int channels = output_shape[1];
+    int height = output_shape[2];
+    int width = output_shape[3];
+
+    float pred_max_value = FLT_MIN;
+    float out_max_value = FLT_MIN;
+    for(int b = 0; b<batch_size;++b){
+      for(int c = 0; c<channels;++c){
+        for(int h = 0; h<height;++h){
+          for(int w = 0; w<width;++w){
+            pred_max_value = std::max(pred_max_value, model_out[b][c][h][w]);
+            out_max_value = std::max(out_max_value, output[b][c][h][w]);
+          }
+        }
+      }
+    }
+
+    printf("pred_max_value is: %f, out_max_value is: %f\n", pred_max_value, out_max_value);
+
+    return areCanvasOutputsClose(model_out, output, 1e-1);
+  }
+  catch (const std::exception& e) {
+    std::cerr << e.what() << std::endl;
+    return false;
+  }
+
+}
+
+bool test_lidarsegdecoder(const std::string& model, const std::string& precision, const std::string& data) {
+  try{
+    // Dummy data
+    auto input_data = nv::Tensor::load(nv::format("%s/lidar_features.tensor", data.c_str()), false);
+    auto output_data = nv::Tensor::load(nv::format("%s/head_sig_input.tensor", data.c_str()), false);
+    std::vector<int64_t> input_shape = input_data.shape;
+    std::vector<int64_t> output_shape = output_data.shape;
+    for(int i = 0; i < input_shape.size(); i++){
+      printf("input_shape[%d] is: %ld\n", i, input_shape[i]);
+    }
+    for(int i = 0; i < output_shape.size(); i++){
+      printf("output_shape[%d] is: %ld\n", i, output_shape[i]);
+    }
+    const nvtype::half* lidar_bevfeat = input_data.ptr<nvtype::half>();
+
+    // Load model
+    const std::string lidardecoder_param = nv::format("model/%s/build/lidar_decoder.plan", model.c_str());
+    std::shared_ptr<bevfusion::fuser::LidarDecoder> lidardecoder_ = bevfusion::fuser::create_lidardecoder(lidardecoder_param);
+
+    //Forward pass
+    cudaStream_t _stream;
+    cudaStreamCreate(&_stream);
+
+    const nvtype::half* lidardecoder_feature = lidardecoder_->forward(lidar_bevfeat, _stream);
+    checkRuntime(cudaStreamSynchronize(_stream));
+    cudaStreamDestroy(_stream);
+
+    // Compare output
+    bevfusion::head::mapsegm::CanvasOutput model_out = copyToCanvasOutput(lidardecoder_feature, output_shape);
+    bevfusion::head::mapsegm::CanvasOutput output = copyToCanvasOutput(output_data.ptr<nvtype::half>(), output_shape);
+    int batch_size = output_shape[0];
+    int channels = output_shape[1];
+    int height = output_shape[2];
+    int width = output_shape[3];
+
+    float pred_max_value = FLT_MIN;
+    float out_max_value = FLT_MIN;
+    for(int b = 0; b<batch_size;++b){
+      for(int c = 0; c<channels;++c){
+        for(int h = 0; h<height;++h){
+          for(int w = 0; w<width;++w){
+            pred_max_value = std::max(pred_max_value, model_out[b][c][h][w]);
+            out_max_value = std::max(out_max_value, output[b][c][h][w]);
+          }
+        }
+      }
+    }
+    printf("pred_max_value is: %f, out_max_value is: %f\n", pred_max_value, out_max_value);
+    return areCanvasOutputsClose(model_out, output, 1e-1);
+  }
+  catch (const std::exception& e) {
+    std::cerr << e.what() << std::endl;
+    return false;
+  }
+
 }
 
 bool load_grid_sampler_plugin() {
@@ -162,9 +331,22 @@ int main(int argc, char** argv) {
   printf("Model: %s\n", model);
   printf("Precision: %s\n", precision);
 
-  if (!test_head(model, precision, data)) {
-      printf("Test failed.\n");
-      return -1;
+  // if (!test_seghead(model, precision, data)) {
+  //     printf("Test failed.\n");
+  //     return -1;
+  // }
+
+  if(strcmp(model, "cameramapsegm") == 0) {
+    if (!test_camsegdecoder(model, precision, data)) {
+        printf("Test failed.\n");
+        return -1;
+    }
   }
 
+  if(strcmp(model, "lidarmapsegm") == 0) {
+    if (!test_lidarsegdecoder(model, precision, data)) {
+      printf("Test failed.\n");
+      return -1;
+    }
+  }
 }
